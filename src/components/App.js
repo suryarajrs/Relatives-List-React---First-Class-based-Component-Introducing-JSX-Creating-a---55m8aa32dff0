6328f47b07176e67efe8b6466d142b74508a3ba1
suryarajrs <suryars@outlook.in>
2024-02-21 16:10:22 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,24 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
+        const relatives = [
+            'Uncle',
+            'Aunt',
+            'Grandpa',
+            'Grandma',
+            'Cousin',
+            'Niece',
+            'Nephew'
+        ];
+
+        const relativeItems = relatives.map((relative, index) =>
+        <li key={`relativeListItem${index + 1}`}>{relative}</li>
+    );
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+               {/* Do not remove the main div */
+               <ol key="relativeList">{relativeItems}</ol>}
             </div>
         )
     }
